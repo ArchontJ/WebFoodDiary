a3961cc288f1e97268f97fcdd13571d924dc8e7f
ARCHONT\Archont <jacek.neska91@gmail.com>
2021-07-19 00:51:52 +0200

    new database, mock page

--- website/models.py
+++ website/models.py
@@ -19,3 +19,23 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150))
     notes = db.relationship('Note')
 
+class PRODUCTS(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    USER_ID = db.Column(db.Integer,db.ForeignKey('user.id'))
+    PROD_NAME = db.Column(db.String(124))
+    KCAL = db.Column(db.Float)
+    PROTEIN = db.Column(db.Float)
+    FAT = db.Column(db.Float)
+    CARB = db.Column(db.Float)
+
+class PROD_UNITS(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    UNIT_DESC = db.Column(db.String(48))
+
+class DAIRY(db.Model):
+     ID = db.Column(db.Integer, primary_key=True)
+     USER_ID = db.Column(db.Integer,db.ForeignKey('user.id'))
+     DATE = db.Column(db.DateTime(timezone=True))
+     PROD_ID = db.Column(db.Integer,db.ForeignKey('products.id'))
+     AMOUNT = db.Column(db.Float)
+     UNIT = db.Column(db.Integer,db.ForeignKey('prod_units.id'))
