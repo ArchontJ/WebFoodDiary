e491c651904f150579c61a000e3e54d6eb6c1fdb
ARCHONT\Archont <jacek.neska91@gmail.com>
2021-07-24 00:58:45 +0200

    db modification

--- website/models.py
+++ website/models.py
@@ -2,11 +2,25 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data=db.Column(db.String(20000))
-    date=db.Column(db.DateTime(timezone=True),default=func.now())
-    user_id=db.Column(db.Integer,db.ForeignKey('USER.ID'))
+class USER(db.Model, UserMixin):
+    ID = db.Column(db.Integer, primary_key=True)
+    EMAIL = db.Column(db.String(150),unique=True)
+    PASSWORD = db.Column(db.String(16))
+    USERNAME = db.Column(db.String(150))
+    PRODUCTS = db.relationship('Products')
+
+class PRODUCT(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    USER_ID = db.Column(db.Integer,db.ForeignKey('USER.ID'))
+    PROD_NAME = db.Column(db.String(124))
+    KCAL = db.Column(db.Float)
+    PROTEIN = db.Column(db.Float)
+    FAT = db.Column(db.Float)
+    CARB = db.Column(db.Float)
+
+class PROD_UNITS(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    UNIT_DESC = db.Column(db.String(48))
 
 class DAIRY(db.Model):
      ID = db.Column(db.Integer, primary_key=True)
@@ -16,26 +30,43 @@ class DAIRY(db.Model):
      AMOUNT = db.Column(db.Float)
      UNIT = db.Column(db.Integer,db.ForeignKey('PROD_UNITS.ID'))
 
-class PROD_UNITS(db.Model):
-    ID = db.Column(db.Integer, primary_key=True)
-    UNIT_DESC = db.Column(db.String(48))
-    dairies = db.relationship('Dairy')
 
-class PRODUCTS(db.Model):
-    ID = db.Column(db.Integer, primary_key=True)
-    USER_ID = db.Column(db.Integer,db.ForeignKey('USER.ID'))
-    PROD_NAME = db.Column(db.String(124))
-    KCAL = db.Column(db.Float)
-    PROTEIN = db.Column(db.Float)
-    FAT = db.Column(db.Float)
-    CARB = db.Column(db.Float)
-    dairies = db.relationship('Dairy')
 
-class USER(db.Model, UserMixin):
-    ID = db.Column(db.Integer, primary_key=True)
-    EMAIL = db.Column(db.String(150),unique=True)
-    PASSWORD = db.Column(db.String(16))
-    USERNAME = db.Column(db.String(150))
-    NOTES = db.relationship('Note')
-    PRODUCTS=db.relationship('Products')
-    DAIRIES = db.relationship('Dairy')
+# class Note(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     data=db.Column(db.String(20000))
+#     date=db.Column(db.DateTime(timezone=True),default=func.now())
+#     user_id=db.Column(db.Integer,db.ForeignKey('USER.ID'))
+
+
+# class PRODUCTS(db.Model):
+#     ID = db.Column(db.Integer, primary_key=True)
+#     USER_ID = db.Column(db.Integer,db.ForeignKey('USER.ID'))
+#     PROD_NAME = db.Column(db.String(124))
+#     KCAL = db.Column(db.Float)
+#     PROTEIN = db.Column(db.Float)
+#     FAT = db.Column(db.Float)
+#     CARB = db.Column(db.Float)
+#     dairies = db.relationship('Dairy')
+    
+# class PROD_UNITS(db.Model):
+#     ID = db.Column(db.Integer, primary_key=True)
+#     UNIT_DESC = db.Column(db.String(48))
+#     dairies = db.relationship('Dairy')
+
+# class USER(db.Model, UserMixin):
+#     ID = db.Column(db.Integer, primary_key=True)
+#     EMAIL = db.Column(db.String(150),unique=True)
+#     PASSWORD = db.Column(db.String(16))
+#     USERNAME = db.Column(db.String(150))
+#     NOTES = db.relationship('Note')
+#     PRODUCTS=db.relationship('Products')
+#     DAIRIES = db.relationship('Dairy')
+
+# class DAIRY(db.Model):
+#      ID = db.Column(db.Integer, primary_key=True)
+#      USER_ID = db.Column(db.Integer,db.ForeignKey('USER.ID'))
+#      DATE = db.Column(db.DateTime(timezone=True))
+#      PROD_ID = db.Column(db.Integer,db.ForeignKey('PRODUCTS.ID'))
+#      AMOUNT = db.Column(db.Float)
+#      UNIT = db.Column(db.Integer,db.ForeignKey('PROD_UNITS.ID'))
